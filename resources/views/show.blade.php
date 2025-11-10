53e6247c0eb2ef920390b20dd340b63fafc603c4
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-10 12:47:51 +0300

    Remove debug line from show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,3 @@
-<p>DEBUG: photo_path = "{{ $submission->photo_path }}"</p>
-
 @if(!empty($submission->photo_path))
     <img src="{{ asset('storage/' . $submission->photo_path) }}" style="max-width: 100%;">
 @else
