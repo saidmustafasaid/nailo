532d91f136827b7d99e2541ca05389238d8c82e5
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-10 19:54:16 +0300

    Remove debug line from show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,5 @@
-@if(!empty($submission->photo_path))
-    <img src="{{ asset('storage/' . $submission->photo_path) }}" style="max-width: 100%;">
+@if(!empty($submission->photo_path) && file_exists(storage_path('app/public/' . $submission->photo_path)))
+    <img src="{{ asset('storage/' . $submission->photo_path) }}" style="max-width: 100%; height: auto;">
 @else
     <p>No photo available.</p>
 @endif
