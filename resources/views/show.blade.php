ae94f2e4cb73b85e6ea8ac89b804d49883f23bdf
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-10 11:28:33 +0300

    Update show.blade.php to display image only

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,20 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
-<h2>Submission Details</h2>
-
-<p><strong>Name:</strong> {{ $submission->name }}</p>
-<p><strong>Phone:</strong> {{ $submission->phone }}</p>
-<p><strong>Location:</strong> {{ $submission->location }}</p>
-<p><strong>Kgs:</strong> {{ $submission->kgs }} kg</p>
-<p><strong>Date:</strong> {{ $submission->created_at->format('M d, H:i') }}</p>
-
-@if($submission->photo)
-    <p><strong>Photo:</strong></p>
-    <img src="{{ asset('storage/' . $submission->photo) }}" style="max-width: 300px;">
+@if($submission->photo && $submission->photo !== 'N/A')
+    <img src="{{ asset('storage/' . $submission->photo) }}" style="max-width: 100%;">
 @else
-    <p><strong>Photo:</strong> N/A</p>
+    <p>No photo available.</p>
 @endif
-
-<a href="{{ route('admin.submissions') }}">Back to list</a>
-@endsection
