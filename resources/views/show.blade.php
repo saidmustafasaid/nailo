52042adfbc4957e8efa19e97a55a7616ead33460
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-10 12:25:01 +0300

    Fix image display by using correct photo_path column

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,6 @@
-@if($submission->photo && $submission->photo_path !== 'N/A')
+<p>DEBUG: photo_path = "{{ $submission->photo_path }}"</p>
+
+@if(!empty($submission->photo_path))
     <img src="{{ asset('storage/' . $submission->photo_path) }}" style="max-width: 100%;">
 @else
     <p>No photo available.</p>
