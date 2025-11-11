9c53dcab0925d47e3fb68a7567eeb57c7a955add
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-11 12:30:58 +0300

    Display submission images from Supabase Storage

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,5 @@
-@if($submission->photo_url)
-    <img src="{{ $submission->photo_url }}" style="max-width:100%; height:auto;">
+@if(!empty($submission->photo_path))
+    <img src="https://ypboqeeentntlesfufxl.supabase.co/storage/v1/object/public/submissions/{{ urlencode($submission->photo_path) }}" style="max-width:100%; height:auto;">
 @else
     <p>No photo available.</p>
 @endif
