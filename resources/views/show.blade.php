af27ea85fa2d3af1846baf4b9dc59eb007213e70
Said Mustafa <saidmustafa@Saids-MacBook-Pro.local>
2025-11-11 12:05:38 +0300

    Integrate Supabase storage for submission images

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,5 @@
-@if(!empty($submission->photo_url))
-    <img src="{{ $submission->photo_url }}" style="max-width: 100%; height: auto;">
-@elseif(!empty($submission->photo_path) && file_exists(storage_path('app/public/' . $submission->photo_path)))
-    <img src="{{ asset('storage/' . $submission->photo_path) }}" style="max-width: 100%; height: auto;">
+@if($submission->photo_url)
+    <img src="{{ $submission->photo_url }}" style="max-width:100%; height:auto;">
 @else
     <p>No photo available.</p>
 @endif
